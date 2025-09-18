cf2fc3d953d54aaa50fc8943405c321578de2585
NKID00 <nkid00@nk0.me>
2025-09-18 09:43:05 +0800

    test: merge crashes/130395.rs into ui/suggestions/incorrect-variant-single-letter.rs

--- tests/ui/suggestions/incorrect-variant-single-letter.rs
+++ tests/ui/suggestions/incorrect-variant-single-letter.rs
@@ -2,10 +2,15 @@
 
 enum A {
     B(),
+    C(i32),
+    D(isize, usize),
 }
 
 fn foo(_: A) {}
 
 fn main() {
-    foo(A::C); //~ ERROR no variant or associated item named `C` found for enum `A` in the current scope [E0599]
+    // Suggestion could be improved to indicate letter case confusion
+    foo(A::b); //~ ERROR no variant or associated item named `b` found for enum `A` in the current scope [E0599]
+    foo(A::c); //~ ERROR no variant or associated item named `c` found for enum `A` in the current scope [E0599]
+    foo(A::d); //~ ERROR no variant or associated item named `d` found for enum `A` in the current scope [E0599]
 }
