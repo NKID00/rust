976ada3f4d3dbb4f42ee4df5798542bc1bb8236e
NKID00 <nkid00@nk0.me>
2025-09-19 18:18:43 +0800

    fix: ignore empty span of elided lifetime arg, fixes #146706

--- tests/ui/suggestions/incorrect-variant-single-letter.rs
+++ tests/ui/suggestions/incorrect-variant-single-letter.rs
@@ -1,16 +1,25 @@
-// Regression test for #146261
+// Regression test for #130395 and #146261
 
 enum A {
-    B(),
-    C(i32),
-    D(isize, usize),
+    B,
+    C(),
+    D(i32),
 }
 
-fn foo(_: A) {}
+fn foo(_: A) -> impl Fn() {
+    || {}
+}
 
 fn main() {
     // Suggestion could be improved to indicate letter case confusion
     foo(A::b); //~ ERROR no variant or associated item named `b` found for enum `A` in the current scope [E0599]
     foo(A::c); //~ ERROR no variant or associated item named `c` found for enum `A` in the current scope [E0599]
     foo(A::d); //~ ERROR no variant or associated item named `d` found for enum `A` in the current scope [E0599]
+    (A::b)(); // FIXME: Suggestion is malformed
+    //~^ ERROR no variant or associated item named `b` found for enum `A` in the current scope [E0599]
+    (A::c)(); //~ ERROR no variant or associated item named `c` found for enum `A` in the current scope [E0599]
+    (A::d)(); //~ ERROR no variant or associated item named `d` found for enum `A` in the current scope [E0599]
+    foo(A::b)(); //~ ERROR no variant or associated item named `b` found for enum `A` in the current scope [E0599]
+    foo(A::c)(); //~ ERROR no variant or associated item named `c` found for enum `A` in the current scope [E0599]
+    foo(A::d)(); //~ ERROR no variant or associated item named `d` found for enum `A` in the current scope [E0599]
 }
